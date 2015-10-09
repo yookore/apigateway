95d7074363e19702bca56e7b7e4d60277a96f4bc
charyorde <dreyemi@gmail.com>
2015-10-09 11:46:47 +0200

    cleanup

--- app.js
+++ app.js
@@ -40,19 +40,6 @@ rateLimitingParams.forEach(function(param) {
 })
 app.use(require('./strong-gateway/server/middleware/rate-limiting/metrics-limiter')(rateLimit))
 
-/*app.use(proxy({
-  "rules": [
-    "^/auth/(.*)$ " + config.auth.uri + "$1 [P]",
-    "^/statics/(.*)$ " + config.statics.uri + "$1 [P]",
-    "^/api/(.*)$ " + config.content.uri + "$1 [P]",
-    "^/stream/(.*)$ " + config.activities.uri + "$1 [P]",
-    "^/audio/(.*)$ " + config.audio.uri + "$1 [P]",
-    "^/media/(.*)$ " + config.media.uri + "$1 [P]",
-    "^/info/(.*)$ " + config.search.uri + "$1 [P]",
-    "^/data/(.*)$ " + config.audiodata.uri + "$1 [P]"
-  ]
-}));*/
-
 app.get('/', function(req, res) {
   res.send("Http Proxy running successfully")
 })
